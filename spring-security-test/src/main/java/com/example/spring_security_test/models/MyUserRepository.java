8149add111e2d61c5a959078cfec3999e68cadd8
Pangpons <pitipongpoomipong@gmail.com>
2025-10-03 18:51:24 +0700

    delete

--- spring-security-test/src/main/java/com/example/spring_security_test/models/MyUserRepository.java
+++
@@ -1,4 +0,0 @@
-package com.example.spring_security_test.models;
-
-public class MyUserRepository {
-}
--- spring_security/src/main/java/com/example/spring_security/controllers/AuthController.java
+++
@@ -1,4 +0,0 @@
-package com.example.spring_security.controllers;
-
-public class AuthController {
-}
